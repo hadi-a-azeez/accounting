aa30541d37dc14d07081ceb3adb99d52620df3f4
hadi-a-azeez <hadikoduvally@gmail.com>
2021-02-18 16:04:47 +0530

    change

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-//export const apiRoot = "https://fliqapp.xyz/acc";
-export const apiRoot = "http://localhost:5000/acc";
+export const apiRoot = "https://wecollab.club/acc";
+//export const apiRoot = "http://localhost:5000/acc";
